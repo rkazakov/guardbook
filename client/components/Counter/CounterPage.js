eaa7f6d9c0c15784b761e67961b1ec3d7e261c55
Ruslan Kazakov <Ruslan.Kazakov@myob.com>
2017-04-18 23:24:56 +1000

    Adding Counter component

---
+++ client/components/Counter/CounterPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const CounterPage = () => {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>This is counter page...</p>
+    </div>
+  )
+}
+
+export default CounterPage;
--- client/components/Header.js
+++ client/components/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" ᛫ "}
+      <Link to="/counter" activeClassName="active">Counter</Link>
+      {" ᛫ "}
       <Link to="/about" activeClassName="active">About</Link>
     </nav>
   );
